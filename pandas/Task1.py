56be5d2ad554adacce28b749228591627e763f16
Agliullin-Ratmir <agliullin.ratmir@gmail.com>
2025-07-13 17:24:11 +0300

    Add pandas homework: add better version to Task1.

--- pandas/Task1.py
+++ pandas/Task1.py
@@ -4,24 +4,16 @@ from datetime import timedelta
 df = pd.read_csv('log.csv')
 
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d_%H:%M:%S')
-print(df)
+
 specific_date1 = pd.to_datetime('2020-04-19').date()
 specific_date2 = pd.to_datetime('2020-04-20').date()
 
 filtered_df = df[(df['date'].dt.date == specific_date1) | (df['date'].dt.date == specific_date2)]
 
 df = filtered_df.sort_values('date').reset_index(drop=True)
-print(df)
 delta1 = timedelta(minutes=30)
 
-# grouped_dict = {name: group for name, group in df.groupby('user')}
-# print(filtered_df)
-
 grouped_dict = df.groupby('user')['date'].apply(list).to_dict()
-#print(grouped_dict)
-# result_intervals = df[df['date'].diff() < delta1]
-# print(result_intervals)
-# print(f"Result: {len(result_intervals)}")
 
 sessions = []
 valid_values = []
@@ -32,7 +24,6 @@ for key in grouped_dict:
 
 for el in valid_values:
     session_list = []
-  #  print(el)
     for i in range(0, len(el) - 2):
         for j in range(i, len(el) - 2):
             if el[j] - el[i] < delta1 and el[i].date() == specific_date1:
@@ -45,3 +36,16 @@ for session in sessions:
 print(f"amount of sessions beginning on 2020-04-19: {session_amount}")
 
 # answer: amount of sessions beginning on 2020-04-19: 6573
+
+# solution made with Gregory's help
+print("solution made with Gregory's help")
+df['diff'] = df.groupby('user')['date'].diff()
+
+
+df['sessions'] = (df['diff'] >= pd.Timedelta(minutes=30)) | df['diff'].isna()
+df['sessions_amount'] = df['sessions'].cumsum()
+filtered_sessions_df = df[df['date'].dt.date == specific_date1]
+last_row = filtered_sessions_df.iloc[-1]
+print(f"amount of sessions beginning on 2020-04-19(better version): {last_row['sessions_amount']}")
+# answer: amount of sessions beginning on 2020-04-19(better version): 6271
+
