2e9ae786bd47cc9534755e59a7b406f21a26def9
Agliullin-Ratmir <agliullin.ratmir@gmail.com>
2025-09-26 22:51:28 +0300

    Add docker homework.

---
+++ docker/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+app = FastAPI()
+
+@app.get("/ping", response_class=PlainTextResponse)
+async def get_content():
+    return "pong"
+
+#docker build -t fastapi-app .
+
+
+#docker run -d -p 8000:8000 fastapi-app
